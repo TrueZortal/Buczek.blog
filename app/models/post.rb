60591db4fe3e81cfe124bff32307056ad990d91c
Mateusz Buczek <mtsbck@gmail.com>
2022-10-15 12:48:53 +0200

    v0 added unrefined tabulating logic to the posts view, additional style improvements

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,8 @@
 class Post < ApplicationRecord
   has_many :comments, -> { order created_at: :asc}
   has_one_attached :thumbnail do |attachable|
-    attachable.variant :thumb, resize_to_limit: [200, 100]
+    attachable.variant :thumb, resize_to_limit: [400, 298]
+    attachable.variant :center, resize_to_limit: [800, 602]
   end
   has_rich_text :content
 
