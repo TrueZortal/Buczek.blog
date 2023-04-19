582253dda104a3b0145f2fbf86db28af2d5cfab8
TrueZortal <mtsbck@gmail.comm>
2023-04-19 16:46:29 +0200

    Graphical_update: removed sass, working on the sliding menu

--- app/javascript/controllers/menu_controller.js
+++ app/javascript/controllers/menu_controller.js
@@ -2,11 +2,31 @@ import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
   myNavbar() {
-    var x = document.getElementById("myNavbar");
+    var x = document.getElementsByClassName("myNavbar");
     if (x.className === "navbar") {
       x.className += " responsive";
     } else {
       x.className = "navbar";
     }
+  }
+
+  rollout(e) {
+    var menu = document.getElementsByClassName("menu")[0]
+    if (menu.className === "menu") {
+      menu.className += "-rolledout"
+    } else {
+      menu.className = "menu"
+    }
+    console.log("boom")
+  }
+
+  rollup() {
+    var menu = document.getElementsByClassName("menu-rolledout")[0]
+    if (menu.className === "menu-rolledout") {
+      menu.className = "menu"
+    } else {
+      menu.className += "-rolledout"
+    }
+    console.log("winding")
   }
 }
