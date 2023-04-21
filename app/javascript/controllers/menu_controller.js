e537bdc34766ac8435d269e6abcc7dc39de5ba4e
TrueZortal <mtsbck@gmail.comm>
2023-04-21 21:43:35 +0200

    Graphical_update: popdown menu stage!

--- app/javascript/controllers/menu_controller.js
+++ app/javascript/controllers/menu_controller.js
@@ -6,6 +6,7 @@ export default class extends Controller {
     setTimeout(() => {
       for (let box of boxes) {
         box.className += '_inplace';
+        box.dataset.action = 'click->menu#rollup'
       }
     }, 3000)
   }
@@ -13,15 +14,19 @@ export default class extends Controller {
   removeAnimation(e) {
     if (e.currentTarget.className != 'link_box_inplace') {
       e.currentTarget.className += '_inplace';
-      e.currentTarget.dataset.action = 'mouseover->menu#freezeFrame'
+      e.currentTarget.dataset.action = 'click->menu#rollup'
     }
   }
 
-  freezeFrame(e) {
-    console.log(e.currentTarget)
-    var box = e.currentTarget
-    var box_pox = box.getBoundingClientRect()
-    console.log(box_pox)
+  rollup(e) {
+    var box = document.getElementsByClassName('menu')[0]
+    var banner = document.getElementsByClassName('banner')[0]
+    box.className += '_floatingoff'
+    banner.className += '_floatingoff'
 
+    setTimeout(() => {
+      box.className = 'menu_static'
+      banner.style.opacity = '0%'
+    }, 2000)
   }
 }
