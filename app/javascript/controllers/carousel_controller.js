aaf28f125dfef949073573912ed7abb2e4ed4583
TrueZortal <mtsbck@gmail.comm>
2023-05-17 21:35:14 +0200

    graphical_update: ready to deploy, fingers crossed

--- app/javascript/controllers/carousel_controller.js
+++ app/javascript/controllers/carousel_controller.js
@@ -9,12 +9,24 @@ export default class extends Controller {
   scrollRight(e) {
     let carousel = document.getElementById("posts_car");
     let post = document.getElementsByClassName("post_box")[0];
+    let back = document.getElementById("posts_backwards")
+    let forward = document.getElementById("posts_forward")
     let current_right = Number(window.getComputedStyle(carousel).getPropertyValue("right").match(/\d+/)[0]);
     let movement_amount = Number(window.getComputedStyle(post).getPropertyValue("width").match(/\d+/)[0]);
     let car_width = carousel.scrollWidth;
     let move = current_right + movement_amount;
-    if (move < car_width/2)
-     carousel.style.right = `${move}px`;
+    if (move > 0) {
+      back.style.display = "flex" ;
+    }
+    if (move <= car_width/2 + movement_amount) {
+      carousel.style.right = `${move}px`;
+    }
+    if (move >= car_width/2 ) {
+      forward.style.display = "none";
+    }
+      console.log(`${current_right} current position`)
+      console.log(`${move} move amount`)
+      console.log(`${car_width/2 + movement_amount} stoppage`)
     }
 
   scrollLeft(e) {
@@ -22,10 +34,15 @@ export default class extends Controller {
     let post = document.getElementsByClassName("post_box")[0];
     let movement = Number(window.getComputedStyle(post).getPropertyValue("width").match(/\d+/)[0]);
     let current_right = Number(window.getComputedStyle(carousel).getPropertyValue("right").match(/\d+/)[0]);
+    let back = document.getElementById("posts_backwards")
+    let forward = document.getElementById("posts_forward")
+    let car_width = carousel.scrollWidth;
     if (current_right - movement > 0) {
       carousel.style.right = `${current_right - movement}px`;
+      forward.style.display = "flex";
     } else {
       carousel.style.right = `0px`;
+      back.style.display = "none" ;
     }
   }
 }
---
+++ app/javascript/controllers/cookies_controller.js
@@ -0,0 +1,20 @@
+import { Controller } from "@hotwired/stimulus"
+
+export default class extends Controller {
+
+  connect(){
+    let cookies = document.getElementById("cookiebar")
+    if (document.cookie == "cookie=consent") {
+      cookies.style.display = "none";
+    }
+  }
+  disappear(e) {
+    let cookies = document.getElementById("cookiebar")
+    cookies.animate({"opacity":"0%"}, 500)
+    setTimeout(() => {
+      cookies.style.display = "none";
+    },500)
+    document.cookie = "cookie=consent"
+  }
+
+}
