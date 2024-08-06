2e894ad88091bd47a0ba9726837e64c9f6148b39
TrueZortal <mtsbck@gmail.comm>
2024-08-06 20:57:42 +0200

    improvements to the styling

--- app/javascript/controllers/timeline_controller.js
+++ app/javascript/controllers/timeline_controller.js
@@ -7,13 +7,9 @@ export default class extends Controller {
     const detailsElement = event.target.parentElement;
     const contentElement = event.target.nextElementSibling;
 
-    // Chrome sometimes has a hiccup and gets stuck.
     if (contentElement.classList.contains('animation')) {
-      // So we make sure to remove those classes manually,
       contentElement.classList.remove('animation', 'collapsing');
-      // ... enforce a reflow so that collapsing may be animated again,
       void element.offsetWidth;
-      // ... and fallback to the default behaviour this time.
       return;
     }
 
@@ -21,13 +17,11 @@ export default class extends Controller {
       "animationend", cb, {once: true}
     );
 
-    // request an animation frame to force Safari 16 to actually perform the animation
     requestAnimationFrame(() => contentElement.classList.add('animation'));
     onAnimationEnd(() => contentElement.classList.remove('animation'));
 
     const isDetailsOpen = detailsElement.getAttribute('open') !== null;
     if (isDetailsOpen) {
-      // prevent default collapsing and delay it until the animation has completed
       event.preventDefault();
       contentElement.classList.add('collapsing');
       onAnimationEnd(() => {
@@ -40,10 +34,11 @@ export default class extends Controller {
     } else {
 
       detailsElement.scrollIntoView( { alignToTop: true, behavior: 'smooth', inline: 'start' } );
-      // var timelineElement = document.getElementById('body_wrapper');
-      // timelineElement.style.overflowY = 'scroll';
-      // timelineElement.scrollTo( { top: event.target.parentElement.parentElement.parentElement.offsetTop, behavior: 'smooth' } );
     }
   }));
 }
+  scrollDown() {
+    var timelineElement = document.getElementById('body_wrapper');
+    timelineElement.scrollTo( { top: timelineElement.scrollHeight, behavior: 'smooth' } );
+  }
 }
